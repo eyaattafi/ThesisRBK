056fac0030a420daefde8b7d5e6fb01c5632d485
farahkharbech <farah.kh@hotmail.fr>
2024-01-07 23:21:27 +0100

    landing page

---
+++ Front/app/Landing.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Landing  = () => {
+  return (
+    <div className="fixed top-0 left-44 right-0 bottom-0 flex items-center justify-start bg-transparent ">
+      <div className="bg-white p-11 h-96 w-80 rounded shadow-md">
+        <p className=" text-xl text-red-900 mb-4 mt-10"></p>
+       <input /> 
+       <input/>
+       <input/>
+      <button className="bg-orange-950 text-white px-4 py-2 rounded hover:bg-red-500">
+      Find Reservation 
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
