6aa49cc2f557b9a3f23523f467b0b092f05eaef3
salimhanchaoui <s.hanchaoui@yahoo.fr>
2024-01-08 18:52:01 +0100

    reviews done

--- Back/index.js
+++ Back/index.js
@@ -11,6 +11,7 @@ const bidRoute=require('./Routes/bidRoute.js')
 const offersRoute=require('./Routes/offersRoute.js')
 const reservationRoute=require('./Routes/reservationsRoute.js')
 const notificationRoute=require('./Routes/notificationRoute.js')
+const reviewRoute=require('./Routes/reviewRoute.js')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -24,7 +25,7 @@ app.use('/api',bidRoute)
 app.use('/api',offersRoute)
 app.use('/api',reservationRoute)
 app.use('/api',notificationRoute)
-
+app.use('/api',reviewRoute)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
