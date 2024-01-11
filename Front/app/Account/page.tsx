ceb1319ecc3aaa8aeb1418527af571cfee7ce6e9
eya <eyaattafi7@gmail.com>
2024-01-11 22:10:56 +0100

    eya

--- Front/app/Account/page.tsx
+++ Front/app/Account/page.tsx
@@ -1,43 +1,65 @@
-import React from "react"
-import Link from "next/link"
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import PaymentIcon from '@mui/icons-material/Payment';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
-import './Account.css'
+"use client";
+// import useState from "react";
+import "./Account.css";
+import Link from "next/link";
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+import PaymentIcon from '@mui/icons-material/Payment'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications'
 
-
-const Account =()=>{
-  
-       
-      
-    return (
-        <div>
-              <div className="acc">Account</div>
-        <div className="div1"><h1>Hello , im eya attafi ,eyaattafi7@gmail.com . <Link href="/">Go to profile</Link></h1></div>
-        <div className="box">
-        <AccountBoxIcon/>  <h1> 
-           <Link href="/Profile">Personal Info</Link> </h1>
-          Provide personal details , and how we can reach you .
-          </div>
-          <div className="box1">
-            <RemoveRedEyeIcon/>
-            <h1>Privacy and sharing</h1>
-          Manage your personal data, connected services , and data sharing setting.
+const Profile = () => {
+  return (
+    
+    <div className="profile-wrapper">
+      <div className="profile-content">
+        <div className="profile-header">
+          <h2>Account</h2>
+          <p>
+            Hammami Mohamed Amine, hammamimin@gmail.com.
+            <Link href="/">Go To Profile</Link>
+          </p>
+        </div>
+        <div className="profile-container">
+          <div className="card">
+            <div className="icon"><AccountBoxIcon/></div>
+            <div className="desc">
+              <p> Personal Info</p>
+              <p>
+                Provide personel details , and how we can reach you .
+              </p>
             </div>
-            <div className="box2">
-            <PaymentIcon/>
-            <h1>Payments & Payouts</h1>
-          Review payments, payouts ,coupons and gift cards.
-                    </div>
-        <div className="box3">
-            <CircleNotificationsIcon/>
-            <h1>Notification</h1>
-         Choose notification prefecences,and how to be contacted .
-                    </div>
- <div/>
-</div>
-    )
-}
+          </div>
+          <div className="card">
+            <div className="icon"><PaymentIcon/></div>
+            <div className="desc">
+              <p>Paiment & Payouts </p>
+              <p>
+                Review paiment , payouts ,coupons and git cards.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="icon"><RemoveRedEyeIcon/></div>
+            <div className="desc">
+              <p>Privacy & Sharing</p>
+              <p>
+                Manage your personal data , connected , services and data sharing setting .
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="icon"><CircleNotificationsIcon/></div>
+            <div className="desc">
+              <p>Notifications</p>
+              <p>
+              Choose notifications and preferences , and how you want to be contacted .
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Account ;
+export default Profile;
