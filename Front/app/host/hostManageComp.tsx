259d0237bea4a46d36d228edceae94b5273316cd
salimhanchaoui <s.hanchaoui@yahoo.fr>
2024-01-12 09:03:10 +0100

    host manage clicks

--- Front/app/host/hostManageComp.tsx
+++ Front/app/host/hostManageComp.tsx
@@ -1,9 +1,24 @@
+"use client"
+import React,{useState} from "react";
 import Link from "next/link"
 import NewBooking from "./newBooking";
 import CurrentStays from "./currentStays";
 import NextArriving from "./nextArriving";
 import Bids from "./bids";
+
 const HostManage = () => {
+    const [ view,setView]=useState<string>('newbooking')
+    const renderComp=()=>{
+        if (view==="newbooking"){
+            return <NewBooking/>
+        }else  if (view==="current"){
+            return <CurrentStays/>
+        }else  if (view==="next"){
+            return <NextArriving/>
+        }else  if (view==="bids"){
+            return <Bids/>
+        }
+    }
     return ( 
     <div className="w-[1100px]">
         <div className="flex flex-row justify-between items-center">
@@ -12,13 +27,13 @@ const HostManage = () => {
         <div className="flex flex-col mt-[50px]">
             <h1 className="text-xl">Your reservations</h1>
             <div className="flex flex-row justify-between mt-[20px]">
-                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white">New Bookings</button>
-                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white">Current Stays</button>
-                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white">Next Arriving</button>
-                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white">Bids</button>
+                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white" onClick={()=>setView('newbooking')}>New Bookings</button>
+                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white" onClick={()=>setView('current')}>Current Stays</button>
+                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white" onClick={()=>setView('next')}>Next Arriving</button>
+                <button className="w-[150px] border border-orange-950 rounded-[10px] hover:bg-orange-950 hover:text-white" onClick={()=>setView('bids')}>Bids</button>
             </div>
         </div>
-        <div className="w-full h-[500px] bg-slate-50 border rounded-[10px] shadow-inner mt-[30px] mb-2 overflow-y-auto"><Bids/></div>
+        <div className="w-full h-[500px] bg-slate-50 border rounded-[10px] shadow-inner mt-[30px] mb-2 overflow-y-auto">{renderComp()}</div>
     </div>
      );
 }
