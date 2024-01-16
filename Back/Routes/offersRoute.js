46cd6c25d8f1f1cce24cf3556479d7a834478851
farahkharbech <farah.kh@hotmail.fr>
2024-01-16 21:42:59 +0100

    first try chart

--- Back/Routes/offersRoute.js
+++ Back/Routes/offersRoute.js
@@ -4,6 +4,7 @@ const Offers=require('../Controllers/offerCont.js')
 
 offersRouter.get('/getAllOffers',Offers.getOffers);
 offersRouter.get('/getAllOffer/:offerid',Offers.getOneOffer);
+
 offersRouter.post('/addOffer',Offers.createOffer);
 offersRouter.put('/updateOffer/:offerid',Offers.updateOffer);
 offersRouter.delete('/deleteOffer/:offerid',Offers.deleteOffer);
