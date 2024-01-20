6879b93db2369c2bd2e9924a5d1a3423c2a5e0c5
farahkharbech <farah.kh@hotmail.fr>
2024-01-20 23:13:02 +0100

    survey in progress

--- Front/app/layout.tsx
+++ Front/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Satisfaction from './Satisfaction'
 import { DataProvider } from "./context"
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +25,7 @@ export default function RootLayout({
         
         {children}
         
-        
+        <Satisfaction/>
        <Footer/> 
         </body>
         </DataProvider>
