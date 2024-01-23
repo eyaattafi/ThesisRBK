f8b594c548a06e1608cb469c1c316bd68d11c4ef
farahkharbech <farah.kh@hotmail.fr>
2024-01-23 10:08:58 +0100

    final version for week3

--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -76,16 +76,16 @@ export default function Home() {
     
    {context?.allOffers.slice(0,4).map((el,i)=>{
     return(
-      <div className="w-[250px] h-[350px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={i}>
+      <div className="w-[250px] h-[350px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden" key={i}>
                            
-                  {/* <Slide>         
+                  <Slide>         
                       {el.offerImages.map((slideImage, index)=> (
                           <Link href="/details">
                           <img className="rounded  w-full h-[200px]" src={slideImage}/>
                           <p>{``}</p>
                           </Link>
                       ))} 
-                  </Slide> */}
+                  </Slide>
               
               <div className="p-2">
 
