eef3f7786460763f0a14fa7727e43c9bc8ad4368
salimhanchaoui <s.hanchaoui@yahoo.fr>
2024-01-24 14:26:17 +0100

    notification fixes

--- Back/database/index.js
+++ Back/database/index.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 
   "thesisrbk",
   "root",
-  "farahkh",
+  "rivenlol00",
 
   {
     host: "localhost",
--- Front/app/details/offerMap.tsx
+++ Front/app/details/offerMap.tsx
@@ -1,7 +1,7 @@
 import Map from "../map/Map";
 const OfferMap = () => {
     return ( 
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 mb-12">
         <h1 className="font-bold text-xl">Where you'll be</h1>
         <div className="ml-[100px] w-[1000px] h-[500px] bg-green-300 border rounded-[10px]">
             <Map/>
